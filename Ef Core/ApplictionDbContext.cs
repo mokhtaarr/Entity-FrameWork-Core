585c1f78db9252e0d877e6e3878bde05e6ddc170
mokhtar ehab <ehabmokhtar32@gmail.com>
2022-12-11 23:30:44 +0200

    Add connection string in Appliction DbContext

---
+++ Ef Core/ApplictionDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ef_Core
+{
+    public class ApplictionDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=DESKTOP-AD4AI73\\MSSQLSERVER01;Initial Catalog=Ef Core;Integrated Security=True;");
+        }
+    }
+}
